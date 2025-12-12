7b7cc77cf2ed47b91d7e4032c9f0154a477861f5
Remi Cloarec <remi.cloarec@outlook.fr>
2025-12-12 15:51:48 +0100

    renamed:    Examples/kuramoto_TREG_dim_V2.py -> Examples/kuramoto.py 	deleted:    Examples/kuramoto_TREG.py 	deleted:    Examples/kuramoto_TREG_dim.py 	renamed:    Examples/lorenz_attractor.py -> Examples/lorenz.py

--- Examples/kuramoto.py
+++ Examples/kuramoto.py
@@ -3,12 +3,21 @@
 Kuramoto model
 ================
 
+Author: R.CLOAREC
+Date: 12/12/2025
+
+Info: this file implements the example use of m-tensor
+for dynamical system learning on the case of Kuramoto
+oscillators.
+
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.linalg import solve_triangular
+import time
 
-import tensorop as to
+import mtensor as mt
 
 #============ UTILS ================================
 
@@ -37,6 +46,7 @@ n_trials = 5
 
 PLOT_SVD = False
 DIST = False
+SAVEFIGS = False
 
 
 for i in range(len(dims)):
@@ -92,7 +102,7 @@ for i in range(len(dims)):
       psi_1 = lambda x: np.array([1., sf*np.cos(x)])
       psi_2 = lambda x: np.array([1., sf*np.sin(x)])
 
-      phi = to.Tensor([np.array([psi_1(X[k, i]) for k in range(m)]) for i in range(dim)]+[np.array([psi_2(X[k, i]) for k in range(m)]) for i in range(dim)])
+      phi = mt.MTensor([np.array([psi_1(X[k, i]) for k in range(m)]) for i in range(dim)]+[np.array([psi_2(X[k, i]) for k in range(m)]) for i in range(dim)])
 
       P = phi@phi
 
@@ -109,15 +119,14 @@ for i in range(len(dims)):
 
       def f(x):
 
-         phi_x = to.Tensor([psi_1(x_i) for x_i in x]+[psi_2(x_i) for x_i in x])
+         phi_x = mt.MTensor([psi_1(x_i) for x_i in x]+[psi_2(x_i) for x_i in x])
 
          return ((phi_x@phi)@Z)[0]
 
-      # ===================== SUBSAMPLING LANDO-LIKE ============
+      # ===================== SUBSAMPLING ALID ============
 
       print(f'{' Here ALID ':=^80}')
 
-      import time
 
       data = X[:-1]
       dim = data.shape[1]
@@ -129,10 +138,10 @@ for i in range(len(dims)):
       # initialize with first
       data_init = data[0]
 
-      phi_lando = to.Tensor([psi_1(data_init[i]) for i in range(dim)]+[psi_2(data_init[i]) for i in range(dim)])
+      phi_alid = mt.MTensor([psi_1(data_init[i]) for i in range(dim)]+[psi_2(data_init[i]) for i in range(dim)])
 
       # initialize cholesky factor [make it a 2D array]
-      L = np.sqrt((phi_lando@phi_lando))
+      L = np.sqrt((phi_alid@phi_alid))
 
       count = 0
 
@@ -142,17 +151,16 @@ for i in range(len(dims)):
          count += 1
 
          # evaluate phi(sample_i)
-         phi_i = to.Tensor([psi_1(sample[i]) for i in range(dim)]+[psi_2(sample[i]) for i in range(dim)])
+         phi_i = mt.MTensor([psi_1(sample[i]) for i in range(dim)]+[psi_2(sample[i]) for i in range(dim)])
 
          # vector
-         k_i = (phi_lando@phi_i)[:, 0]
+         k_i = (phi_alid@phi_i)[:, 0]
          # scalar
          k_ii = (phi_i@phi_i)[0, 0]
 
          # evaluate coef minimizing dist in feature space
-         # np.linalg.solve will use cholesky automatically
-         s = np.linalg.solve(L, k_i)
-         a = np.linalg.solve(L.T, s)
+         s = solve_triangular(L, k_i, lower=True)
+         a = solve_triangular(L.T, s)
 
          # evaluate dist in feature space
          dist = k_ii - k_i@a
@@ -165,7 +173,7 @@ for i in range(len(dims)):
             msk_rhs.append(True)
 
             # append phi_i to phi
-            phi_lando.append(phi_i.cores, axis=-1)
+            phi_alid.append(phi_i.cores, axis=-1)
 
             # lower entry of L
             c_i = np.max([0., np.sqrt(k_ii-s.T@s)])
@@ -179,28 +187,28 @@ for i in range(len(dims)):
       print('here', count, len(msk_rhs), data.shape)
 
       # once best subset is identified based on given data, TREG
-      m_ = phi_lando.shape[0]
+      m_ = phi_alid.shape[0]
 
       # rhs for weights computation
       Y = Xdt[msk_rhs]
 
       # change this solving method and use L instead THEN how to regularize ?
-      Z_lando = np.linalg.solve(L.T, np.linalg.solve(L, Y))
+      Z_alid = solve_triangular(L.T, solve_triangular(L, Y, lower=True))
 
-      print(f'Shapes for LANDO system: phi_lando: {phi_lando.shape}, Z_lando: {Z_lando.shape}')
+      print(f'Shapes for alid system: phi_alid: {phi_alid.shape}, Z_alid: {Z_alid.shape}')
 
       # online computation
-      def f_lando(x):
+      def f_alid(x):
 
-         phi_x = to.Tensor([psi_1(x_i) for x_i in x]+[psi_2(x_i) for x_i in x])
+         phi_x = mt.MTensor([psi_1(x_i) for x_i in x]+[psi_2(x_i) for x_i in x])
 
-         return ((phi_x@phi_lando)@Z_lando)[0]
+         return ((phi_x@phi_alid)@Z_alid)[0]
 
 
-      check_rhs = phi@phi_lando@Z_lando
+      check_rhs = phi@phi_alid@Z_alid
 
       # should be shit
-      print('Error l2 LANDO',np.linalg.norm(Xdt-check_rhs)/np.linalg.norm(Xdt))
+      print('Error l2 alid',np.linalg.norm(Xdt-check_rhs)/np.linalg.norm(Xdt))
 
 
       #===================== BUILD TENSOR REGRESSION =============
@@ -254,7 +262,7 @@ for i in range(len(dims)):
       print(f'time reconstructing TENSOR FULL RANK trajectory: {time.perf_counter() - t1}s')
       print(f'ie unitary cost for f_: {(time.perf_counter() - t1)/num_steps_test}s')
 
-      # ------------ TENSOR LANDO SUBSAMPLING --------------
+      # ------------ TENSOR alid SUBSAMPLING --------------
 
       # re-generate data from initial point using tensor integrator
       X_ALI = np.empty((num_steps_test + 1, dim))  # Need one more for the initial values
@@ -269,18 +277,18 @@ for i in range(len(dims)):
       # and using them to estimate the next point
       for t in range(num_steps_test):
          
-         X_ALI[t + 1] = X_ALI[t] + f_lando(X_ALI[t]) * dt
+         X_ALI[t + 1] = X_ALI[t] + f_alid(X_ALI[t]) * dt
 
       # timer for reconstruction
-      print(f'time reconstructing TENSOR LANDO trajectory: {time.perf_counter() - t1}s')
+      print(f'time reconstructing TENSOR alid trajectory: {time.perf_counter() - t1}s')
       print(f'ie unitary cost for f_: {(time.perf_counter() - t1)/num_steps_test}s')
 
       # 
       delta_full_rank = np.linalg.norm(X_ - X, axis=1)/np.linalg.norm(X, axis=1)
-      delta_lando = np.linalg.norm(X_ALI - X, axis=1)/np.linalg.norm(X, axis=1)
+      delta_alid = np.linalg.norm(X_ALI - X, axis=1)/np.linalg.norm(X, axis=1)
 
       plt.plot(range(num_steps_test+1), delta_full_rank, label='full rank')
-      plt.plot(range(num_steps_test+1), delta_lando, label='ALI regul')
+      plt.plot(range(num_steps_test+1), delta_alid, label='ALI regul')
       plt.legend()
       plt.yscale('log')
       plt.xlabel('Timestep')
@@ -291,7 +299,7 @@ for i in range(len(dims)):
       # store to average
       print(k)
       curves_lstsq[k] = delta_full_rank
-      curves_ALI[k] = delta_lando
+      curves_ALI[k] = delta_alid
 
    # here build averaged curves
    plt.fill_between(range(num_steps_test+1), curves_lstsq.max(axis=0), curves_lstsq.min(axis=0), alpha = .5, linewidth=0)
@@ -307,7 +315,7 @@ for i in range(len(dims)):
    plt.ylim((1e-18, 1e2))
    plt.grid(True, which='both')
 
-   plt.savefig(f'img_kuramoto/kuramoto_dim_{dim}.pdf', bbox_inches='tight')
+   if SAVEFIGS : plt.savefig(f'img_kuramoto/kuramoto_dim_{dim}.pdf', bbox_inches='tight')
 
    plt.show()
 
